8ce27bfd81be049a8146158a84f4349ec38ecfd7
Andrew Ashraf <andrewashrafwg97@gmail.com>
2025-08-18 17:09:53 +0300

    static html

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,35 +5,10 @@ console.log(__dirname)
 console.log(__filename)
 
 const publicDirectoryPath = path.join(__dirname, '../public')
-// console.log(path.join(__dirname, '../public'))
-
 const app = express()
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
-
-
-// app.get('', (req, res) => {
-//     res.send('<h1>Weather<h1>')
-// })
-
-// app.get('/help', (req, res) => {
-//     res.send([{
-//         name: 'Andrew',
-//     },
-//     {
-//         name: 'Joe Doe',
-//     }])
-// })
-// app.get('/about', (req, res) => {
-//     res.send('<h1>About Page<h1>')
-// })
-
-// app.get('/weather', (req, res) => {
-//     res.send({
-//         location:"Cairo, EG",
-//         temp: 38
-//     })
-// })
 
 
 //Start Server
