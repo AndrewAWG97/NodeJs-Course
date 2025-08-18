e3b6571c481f6c6460cfb66cda496cace9555cd2
Andrew Ashraf <andrewashrafwg97@gmail.com>
2025-08-18 17:36:59 +0300

    Use HBS instead of html

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -10,6 +10,26 @@ const app = express()
 app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
 
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About',
+        name: 'Andrew'
+    })
+})
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'help',
+        name: 'Andrew'
+    })
+})
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Andrew'
+    })
+})
+
 
 //Start Server
 app.listen(3000, () => {
