e924f00f8601b542b600016cc7827b5441ffb394
Andrew Ashraf <andrewashrafwg97@gmail.com>
2025-08-18 15:52:59 +0300

    Access HTML files

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,23 +1,43 @@
 const express = require('express')
+const path = require('path')
+
+console.log(__dirname)
+console.log(__filename)
+
+const publicDirectoryPath = path.join(__dirname, '../public')
+// console.log(path.join(__dirname, '../public'))
 
 const app = express()
 
-app.get('', (req, res) => {
-    res.send('Hello express!')   
-})
+app.use(express.static(publicDirectoryPath))
 
-app.get('/help', (req, res) => {
-    res.send('Help page!')   
-})
-app.get('/about', (req, res) => {
-    res.send('About page!')   
-})
 
-app.get('/weather', (req, res) => {
-    res.send('View Weather')   
-})
+// app.get('', (req, res) => {
+//     res.send('<h1>Weather<h1>')
+// })
 
-app.listen(3000, () =>{
+// app.get('/help', (req, res) => {
+//     res.send([{
+//         name: 'Andrew',
+//     },
+//     {
+//         name: 'Joe Doe',
+//     }])
+// })
+// app.get('/about', (req, res) => {
+//     res.send('<h1>About Page<h1>')
+// })
+
+// app.get('/weather', (req, res) => {
+//     res.send({
+//         location:"Cairo, EG",
+//         temp: 38
+//     })
+// })
+
+
+//Start Server
+app.listen(3000, () => {
     console.log('Server is up on port 3000')
 })
 
