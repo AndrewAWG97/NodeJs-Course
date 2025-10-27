05673692f74f22bf4e0144d6ca94220b5dcd6c03
Andrew Ashraf <andrewashrafwg97@gmail.com>
2025-10-27 15:43:50 +0300

    Update 9-async-await.js

--- playground/9-async-await.js
+++ playground/9-async-await.js
@@ -1,6 +1,9 @@
 const add = async (a, b) => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
+            if(a < 0 || b < 0) {
+                return reject('Numbers must be non-negative')
+            }
             resolve(a + b)
         }, 2000)
     })
@@ -14,7 +17,7 @@ const add = async (a, b) => {
 const doWork = async () => {
     const sum = await add(1, 99)  // waits until the promise is resolved
     const sum2 = await add(sum, 50)
-    const sum3 = await add(sum2, 3)
+    const sum3 = await add(sum2, -5) // This will cause an error
     return sum3
 }
 
