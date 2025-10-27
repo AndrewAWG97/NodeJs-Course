942955fece855fcb6a70acac960b892df8343433
Andrew Ashraf <andrewashrafwg97@gmail.com>
2025-10-27 15:36:10 +0300

    Add async/await playground example
    Introduces a new file demonstrating async/await usage in JavaScript, including an async add function and a doWork function that chains asynchronous operations.

---
+++ playground/9-async-await.js
@@ -0,0 +1,28 @@
+const add = async (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a + b)
+        }, 2000)
+    })
+}
+
+//async allways returns a promise
+// Promise is fulfilled with the value that is returned from the async function
+
+//await operator can only be used inside an async function
+// It makes JavaScript wait until that promise settles and returns its result.
+const doWork = async () => {
+    const sum = await add(1, 99)  // waits until the promise is resolved
+    const sum2 = await add(sum, 50)
+    const sum3 = await add(sum2, 3)
+    return sum3
+}
+
+
+doWork().then((result) => {
+    console.log("Result:", result)
+}).catch((e) => {
+    console.log("Error:", e)
+})
+
+console.log("After calling doWork")
