448797402ffc6eed7ceb7444bc4d7e56b41093c3
Andrew Ashraf <andrewashrafwg97@gmail.com>
2025-10-27 17:32:44 +0300

    Update index.js

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -8,19 +8,15 @@ const port = process.env.PORT || 3000
 require('./db/mongoose') // Ensure database connection is established
 
 // Import User model
-const User = require('./models/user')
-const Task = require('./models/task')
 const UserRouter = require('./routers/user')
 const TaskRouter = require('./routers/task')
 
 // Middleware to parse JSON bodies
 app.use(express.json())
 
+// Use imported routers
 app.use(UserRouter)
 app.use(TaskRouter)
-
-
-
 
 // Start the server
 app.listen(port, () => {
